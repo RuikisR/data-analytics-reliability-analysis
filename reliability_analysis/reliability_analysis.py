eaafa225a3e782dd986c08d657e49368f0343d6e
Cata Gheorghiu <117468209+ggcacalin@users.noreply.github.com>
2023-03-21 18:30:16 +0000

    Complete (but wrong) basic implementation
    Incorporated broken sublattice identification method, see TODO for what is wrong right now

--- reliability_analysis/reliability_analysis.py
+++ reliability_analysis/reliability_analysis.py
@@ -5,14 +5,6 @@ import math
 # Uptime rate (lam) and downtime rate (mu); they give average periods until event
 lam = 10
 mu = 10
-# Simulation end
-end = 50
-# Lattice dimensions
-m = 3
-n = 3
-r = 2
-s = 2
-
 
 class Node:
     def __init__(self):
@@ -20,7 +12,7 @@ class Node:
         self.__next_event = np.random.exponential(lam)
 
     def update(self, event, current_time):
-        # Boolean event decides if uptime or downtime computed; setter for status and next event
+        #Boolean event decides if uptime or downtime computed; setter for status and next event
         if event:
             self.__status = True
             self.__next_event = current_time + np.random.exponential(lam)
@@ -34,8 +26,33 @@ class Node:
     def getNext(self):
         return self.__next_event
 
+def check_system(r, s, lattice):
+    m = len(lattice)
+    n = len(lattice[0])
+    #Building transformed matrix
+    transformed_matrix = []
+    flag = 0
+    for i in range(m):
+        transformed_row = []
+        for j in range(n + s - 1):
+            #Scan the lattice for dangerous rows, hopping over columns' end
+            if not lattice[i % m][j % n].getStatus():
+                flag += 1
+                if flag == s:
+                    transformed_row.append((j-s+1) % n)
+                    flag -= 1
+            else:
+                flag = 0
+        transformed_matrix.append(transformed_row)
+    #Scan transformed matrix to see if it's broken
+    for row in range(m + r - 1):
+        if (set(transformed_matrix[row % m]) & set(transformed_matrix[(row + 1) % m])):
+            return 0
+    return 1
 
-def main():
+def simulate(m, n, r, s):
+    # Simulation end
+    end = 50
     # Initiate queue of failures and repairs
     event_queue = PriorityQueue()
     # Create latice and fill with nodes, record initial failures
@@ -47,23 +64,36 @@ def main():
     # Initiate system status history
     t = 0
     system_history = []
+    flag = 0
     # Simulation
-    while t < end:
+    while (t < end):
         # Get features of next event
         next_event = event_queue.get()
-        next_time, next_status, next_node = next_event
-        i = next_node // m
+        next_time = next_event[0]
+        next_status = next_event[1]
+        next_node = next_event[2]
+        i = int(next_node / m)
         j = next_node % n
+        # If it's a new period, all nodes of previous one are updated
+        # and system check commences before new updates
+        if (t != math.ceil(next_time)):
+            # TODO: Figure out why sometimes there are more than 50 periods
+            system_status = check_system(r, s, lattice)
+            # Fill in system status for each period without events
+            for k in range(math.ceil(next_time) - t):
+                system_history.append(system_status)
         # Advance clock
         t = min(end, math.ceil(next_time))
         # Update node and event queue
         lattice[i][j].update(not next_status, t)
         event_queue.put((lattice[i][j].getNext(), lattice[i][j].getStatus(), m * i + j))
-        # TODO: Check system integrity
-        system_history.append(t)
     return system_history
 
+def main():
+    system_history = simulate(3, 3, 2, 2)
+    return system_history
 
 if __name__ == "__main__":
     system_history = main()
     print(system_history)
+    print(len(system_history))
